973241d558c55f0cda0b26838f470aeb0616620b
alejandroscf <alejandroscf@gmail.com>
2016-03-19 19:43:06 +0100

    track.php manages cell id parameters

--- tracker.php
+++ tracker.php
@@ -8,26 +8,29 @@
         return;
     }
 
-    $info['lat'] = round($_GET['lat'], $accuracy);
-    $info['lon'] = round($_GET['lon'], $accuracy);
-    $info['timestamp'] = $_GET['timestamp'];
-    $info['hdop'] = $_GET['hdop'];
-    $info['altitude'] = $_GET['altitude'];
-    $info['speed'] = $_GET['speed'];
+    $info['mcc'] = $_GET['mcc'];
+    $info['mnc'] = $_GET['mnc'];
+    $info['lac'] = $_GET['lac'];
+    $info['ci'] = $_GET['ci'];
+    $info['vcell'] = $_GET['vcell'];
+    $info['soc'] = $_GET['soc'];
+    $info['sig'] = $_GET['sig'];
+    $info['timestamp'] = time()*1000;
 
 
     $loc_string = file_get_contents($filePath);
     $old = unserialize($loc_string);
-    $diff = round(abs($info['timestamp']-$old['timestamp'])/1000);
+    //$diff = round(abs($info['timestamp']-$old['timestamp'])/1000);
     $newPath = $gpxDirectory . date("Y-m-d-H-i-s",round($old['timestamp']/1000)) . $logName;
-    if ($diff > 60) {
+    // TODO: Separa el fichero si han pasado ¿24? horas
+    /*if ($diff > 60) {
        rename($logPath, $newPath);
        //TODO Create GPX file
        s2g($newPath,$outString);
        $fh = fopen(str_replace(".log","",$newPath).".gpx", 'w');
        fwrite($fh, $outString);
        fclose($fh);
-    }
+    }*/
     $fh = fopen($logPath, 'a');
     fwrite($fh, serialize($info) . "\n");
     fclose($fh);
